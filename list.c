8646e06d604656aeebb20ef999db03d27a520dd8
hasifahmed <hahmed@stuy.edu>
2018-10-09 15:36:23 -0400

    sad

--- list.c
+++ list.c
@@ -5,15 +5,21 @@
 struct node {int i; struct node *next;};
 
 void print_list(struct node * p){
-  if(n.i != NULL){
-    printf("%d ->", n.1);
-  }else{
+  if(*(p->next) == NULL){
     printf("NULL");
-  }
+  
+  }else{
+    
+      printf("%d ->",p->i);
   print_list(p.next);
+  }
+  
 }
 
 struct node * insert_front(struct node * p, int val){
+  struct node newheader;
+  newheader.i = val;
+  newheader.next = p;
 }
 
 struct node * free_list(struct node * p){
@@ -21,6 +27,16 @@ struct node * free_list(struct node * p){
 
 
 int main(){
+  struct node third;
+  third.i = 4;
+  struct node second;
+  second.i = 3;
+  second.next = &third;
+  struct node first;
+  first.next = &second;
+
+  print_list(first.next);
+ 
   
   return 0;
 }
