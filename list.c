7e789d908eb3b771f5bb9a3d5bf45461334f8581
hasifahmed <hahmed@stuy.edu>
2018-10-09 15:21:50 -0400

    beg

---
+++ list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {int i; struct node *next;};
+
+void print_list(struct node * p){
+  if(n.i != NULL){
+    printf("%d ->", n.1);
+  }else{
+    printf("NULL");
+  }
+  print_list(p.next);
+}
+
+struct node * insert_front(struct node * p, int val){
+}
+
+struct node * free_list(struct node * p){
+}
+
+
+int main(){
+  
+  return 0;
+}
