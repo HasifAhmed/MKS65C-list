f6b635c50a3aa45b433b62d8ade924ab0aaf9c54
hasifahmed <hahmed@stuy.edu>
2018-10-09 23:31:54 -0400

    done

--- list.c
+++ list.c
@@ -5,38 +5,48 @@
 struct node {int i; struct node *next;};
 
 void print_list(struct node * p){
-  if(*(p->next) == NULL){
-    printf("NULL");
   
-  }else{
-    
-      printf("%d ->",p->i);
-  print_list(p.next);
+  if(p->next == NULL){
+    printf("END\n-----------\n");
   }
-  
+  else {
+    struct node *hold = p;
+    printf("%d -> \n", hold -> i);
+    print_list(hold -> next);
+  }
+
 }
 
 struct node * insert_front(struct node * p, int val){
-  struct node newheader;
-  newheader.i = val;
-  newheader.next = p;
+  struct node *add  = (struct node *)malloc(sizeof(struct node));
+  add->next = p;
+  add->i = val;
+  return add;
 }
 
 struct node * free_list(struct node * p){
+  struct node * hold = p;
+  struct node *temp = p;
+  while(hold -> next){
+    free(temp);
+    hold = hold -> next;
+    temp = hold;
+  }
+  free(temp);
+  return p;
 }
 
 
 int main(){
-  struct node third;
-  third.i = 4;
-  struct node second;
-  second.i = 3;
-  second.next = &third;
-  struct node first;
-  first.next = &second;
+  struct node *node1 = malloc(sizeof(struct node));
+  node1 = insert_front(node1, 2);
+  print_list(node1);
+  node1 = insert_front(node1, 4);
+  print_list(node1);
+  free_list(node1);
 
-  print_list(first.next);
- 
-  
+
+
+
   return 0;
 }
